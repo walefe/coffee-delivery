6c6764912dd0d1fa92dfa0816c0d57c75447d16b
Walefe <walefedev@gmail.com>
2023-01-16 17:02:02 -0300

    chore: install styled-components and create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <h1>Coffee  Delivery</h1>
+    <>
+      <h1>Coffee  Delivery</h1>
+      <GlobalStyle />
+    </>
+
   )
 }
