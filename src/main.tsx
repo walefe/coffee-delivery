e37428f7fd94de66f44017d0aade256d84c8110a
Walefe <walefedev@gmail.com>
2023-01-28 10:06:42 -0300

    feat: config msw for mocking api

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { worker } from './mocks/browser'
 import { App } from './App'
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
