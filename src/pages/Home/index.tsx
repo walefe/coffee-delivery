ca4a5843d5bab1011c323523a61647727ea2807a
Walefe <walefedev@gmail.com>
2023-02-05 15:14:06 -0300

    created page home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+import { Container } from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <Header />
+      <h1>Home Page</h1>
+    </Container>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 90rem;
+  height: 100vh;
+  margin: 0 auto;
+`
