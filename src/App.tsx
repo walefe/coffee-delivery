c06730cb2c1633b46239e10fd62c6118b5463d1f
Walefe <walefedev@gmail.com>
2023-02-05 15:14:36 -0300

    added page home to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Home } from './pages/Home'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Coffee  Delivery</h1>
+      <Home />
       <GlobalStyle />
-      </ThemeProvider>
+    </ThemeProvider>
   )
 }
