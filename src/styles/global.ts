0f7ce4d825dffa3081dd46bb6620c8eb8989f429
Walefe <walefedev@gmail.com>
2023-02-05 15:14:54 -0300

    added some global styles and logo

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,9 +6,14 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   body {
     background-color: ${(props) => props.theme.colors.background};
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    width: 100vw;
+    height: 100vh;
   }
 `
