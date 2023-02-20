c8c3cd75c1eaf87db073f1e8e41611ebe9fe782e
Walefe <walefedev@gmail.com>
2023-02-20 14:30:46 -0300

    feat: created banner to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,31 @@
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import * as S from "./styles";
+
+import CoffeeLogo from '../../assets/coffee.svg'
 
 export function Home() {
   return (
-    <Container>
+    <S.Container>
       <Header />
-      <h1>Home Page</h1>
-    </Container>
+      <S.BannerContainer>
+        <S.ParagraphContainer>
+          <S.Paragraph type="big">
+            Encontre o café perfeito para qualquer hora do dia
+          </S.Paragraph>
+
+          <S.Paragraph type="medium">
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+          </S.Paragraph>
+
+          <div>
+            <S.Paragraph type="small">Compra simples e segura</S.Paragraph>
+            <S.Paragraph type="small">Embalagem mantém o café intacto</S.Paragraph>
+            <S.Paragraph type="small">Entrega rápida e rastreada</S.Paragraph>
+            <S.Paragraph type="small">O café chega fresquinho até você</S.Paragraph>
+          </div>
+        </S.ParagraphContainer>
+        <img src={CoffeeLogo} alt="" />
+      </S.BannerContainer>
+    </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,69 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface ParagraphProps {
+  type: 'small' | 'medium' | 'big'
+}
 
 export const Container = styled.div`
   max-width: 90rem;
   height: 100vh;
   margin: 0 auto;
 `
+
+export const BannerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: inherit;
+  max-width: 100%;
+  height: 34rem;
+  padding: 5.8rem 0;
+`
+
+export const ParagraphContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 36.7rem;
+  gap: 1rem;
+
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin-top: 2rem;
+
+    p + p {
+      margin-bottom: 1rem;
+    }
+  }
+`
+
+export const Paragraph = styled.p<ParagraphProps>`
+  color: ${({ theme }) => theme.colors.base.subtitle};
+  ${({ type }) => wrapperModifiers[type]}
+  line-break: auto;
+`
+const wrapperModifiers = {
+  small() {
+    return css`
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 1.3rem;
+    `
+  },
+  medium() {
+    return css`
+
+      font-weight: 400;
+      font-size: 1.2rem;
+      line-height: 1.6rem;
+    `
+  },
+  big() {
+    return css`
+      font-family: 'Baloo 2';
+      font-size: 3rem;
+      line-height: 3.8rem;
+      font-weight: 800;
+
+    `
+  }
+}
